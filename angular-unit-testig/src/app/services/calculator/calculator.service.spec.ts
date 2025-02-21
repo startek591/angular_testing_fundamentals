325d983faf3dc249c07a73510ce4d216160beccb
jonathan_fambro <startek591@gmail.com>
2025-02-21 14:54:35 -0500

    Before each method

--- angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
+++ angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
@@ -2,18 +2,20 @@ import { LoggerService } from '../logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
-  it('should add two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculatorService = new CalculatorService(mockLoggerService);
-    const result = calculatorService.add(1, 2);
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+  beforeEach(() => {
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+  });
 
+  it('should add two numbers', () => {
+    let result = calculator.add(1, 2);
     expect(result).toBe(3);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    let calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
