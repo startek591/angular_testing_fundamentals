6246fd25cf7842f1f3a73ee69c5fbab9354a4f7e
jonathan_fambro <startek591@gmail.com>
2025-02-21 14:32:50 -0500

    Createed a spy object on logger service - best practice (don't user the actual real service)

--- angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
+++ angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
@@ -3,17 +3,19 @@ import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    let loggerService = new LoggerService();
-    const calculatorService = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculatorService = new CalculatorService(mockLoggerService);
     const result = calculatorService.add(1, 2);
 
     expect(result).toBe(3);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    let loggerService = new LoggerService();
-    let calculator = new CalculatorService(loggerService);
+    let mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    let calculator = new CalculatorService(mockLoggerService);
     let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
