51ea1838d3767add48f9b9aa8a8cb6b97e84a8f8
jonathan_fambro <startek591@gmail.com>
2025-02-21 13:28:32 -0500

    Create first spec file

---
+++ angular-unit-testig/src/app/first-test.spec.ts
@@ -0,0 +1,18 @@
+describe('First test', () => {
+  let testVariable: any;
+
+  beforeEach(() => {
+    testVariable = {};
+  });
+
+  it('should return true if a is true', () => {
+    // arrange
+    testVariable.a = false;
+
+    // act
+    testVariable.a = true;
+
+    // assert
+    expect(testVariable.a).toBe(true);
+  });
+});
