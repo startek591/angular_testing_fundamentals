ce62ef02d72d9daf02db9c639612546331623e15
jonathan_fambro <startek591@gmail.com>
2025-02-21 14:11:24 -0500

    Create logger service

--- angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
+++ angular-unit-testig/src/app/services/calculator/calculator.service.spec.ts
@@ -1,14 +1,18 @@
+import { LoggerService } from '../logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const calculatorService = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculatorService = new CalculatorService(loggerService);
     const result = calculatorService.add(1, 2);
+
     expect(result).toBe(3);
   });
 
   it('should subtract two numbers', () => {
-    let calculator = new CalculatorService();
+    let loggerService = new LoggerService();
+    let calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
   });
