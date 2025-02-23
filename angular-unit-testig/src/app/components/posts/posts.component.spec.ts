6ef44ae2386bd9bde7170ffae19a97cef204a0b6
jonathan_fambro <startek591@gmail.com>
2025-02-23 10:40:43 -0500

    Post component with parent instance

--- angular-unit-testig/src/app/components/posts/posts.component.spec.ts
+++ angular-unit-testig/src/app/components/posts/posts.component.spec.ts
@@ -109,5 +109,22 @@ describe('PostsComponent', () => {
         expect(component.delete).toHaveBeenCalledWith(POSTS[i]);
       }
     });
+
+    it('should call the delete method when the delete event is emitted in Post Component', () => {
+      spyOn(component, 'delete');
+      mockPostService.getPosts.and.returnValue(of(POSTS));
+      fixture.detectChanges();
+
+      let postComponentDEs = fixture.debugElement.queryAll(
+        By.directive(PostComponent)
+      );
+
+      for (let i = 0; i < postComponentDEs.length; i++) {
+        (postComponentDEs[i].componentInstance as PostComponent).delete.emit(
+          POSTS[i]
+        );
+        expect(component.delete).toHaveBeenCalledWith(POSTS[i]);
+      }
+    });
   });
 });
