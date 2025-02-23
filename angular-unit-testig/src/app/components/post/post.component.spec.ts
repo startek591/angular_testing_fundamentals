dfc3d4ee70dcc7bd7259e84b9ff0934ebfbb4f31
jonathan_fambro <startek591@gmail.com>
2025-02-22 21:55:24 -0500

    Post component Create TestBed

--- angular-unit-testig/src/app/components/post/post.component.spec.ts
+++ angular-unit-testig/src/app/components/post/post.component.spec.ts
@@ -1,10 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { first } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { PostComponent } from './post.component';
 
 describe('Post Component', () => {
+  let fixture: ComponentFixture<PostComponent>;
+  let component: PostComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PostComponent],
+    });
+
+    fixture = TestBed.createComponent(PostComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the post component using TestBed', () => {
+    expect(component).toBeDefined();
+  });
+
   it('should raise an event when the delete post is clicked', () => {
-    const component = new PostComponent();
     const post: Post = { id: 1, body: 'body 1', title: 'dsdsd' };
     component.post = post;
     component.delete.pipe(first()).subscribe((selectedPost) => {
